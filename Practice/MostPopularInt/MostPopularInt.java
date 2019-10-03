a044ed1240413d0646f96be358e56dfce7067784
Caston Lambert <Caston@Jessicas-Air.attlocal.net>
2019-10-02 21:14:44 -0500

    MostPopularInt is functioning correctly

--- Practice/MostPopularInt/MostPopularInt.java
+++ Practice/MostPopularInt/MostPopularInt.java
@@ -4,16 +4,50 @@ class MostPopularInt
 {
   public static void main (String[] args)
   {
-    System.out.println("Hello");
+    //System.out.println("Hello");
+
+    int array[] = {2, 1, 2, 3, 4, 2, 3, 1, 1, 1};
+    int n = array.length;
+
+    //System.out.println("");
+
+    //String arr = Arrays.toString(array);
+
+    System.out.println(getElement(array, n));
+
+    //System.out.println(arr);
   }
 
-  puplic static int getElement()
+  static int getElement(int array[], int n)
   {
-    int[] array = new int[] {1, 2, 3, 4, 5, 2, 3, 2};
-
     Arrays.sort(array);
 
     int max_count = 1;
+    int res = array[0];
+    int current_count = 1;
 
+    for (int i = 1; i < n; i++)
+    {
+      if (array[i] == array[i - 1])
+      {
+        current_count++;
+      }
+      else
+      {
+        if (current_count > max_count)
+        {
+          max_count = current_count;
+          res = array[i - 1];
+        }
+        current_count = 1;
+      }
+    }
+
+    if (current_count > max_count)
+    {
+      max_count = current_count;
+      res = array[n - 1];
+    }
+    return res;
   }
 }
